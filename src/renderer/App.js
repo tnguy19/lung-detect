c7dff79f003438e95d5b64fe3a9807ad332de848
ttnguy19 <nguyenthinh19011@gmail.com>
2024-10-18 13:47:14 -0400

    Set up boilerplate for project

---
+++ src/renderer/App.js
@@ -0,0 +1,21 @@
+import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
+import icon from '../../assets/icon.svg';
+import './App.css';
+
+function Hello() {
+  return (
+    <div>
+      <h1>Lung Detect</h1>
+    </div>
+  );
+}
+
+export default function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Hello />} />
+      </Routes>
+    </Router>
+  );
+}
